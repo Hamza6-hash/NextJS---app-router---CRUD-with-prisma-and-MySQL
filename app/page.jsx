c33100dc4f08384188b419a25800cee0ce41d23c
Hamza Ali <mastank387@gmail.com>
2023-09-02 11:52:58 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import UpdateComponent from "@/components/UpdateComponent";
 
 export const dynamic = "force-dynamic";
 
+// this is equivalent to get ServerSideProps or getStaticProp by default every nextJS fetch request uses getStaticProps which will provide build time data to user no matter how much time the user request result will same,so to avoid this we already add a line after the imports or we can add { cache: "no-store" } after the url in fetch method 
 async function getUsers() {
   const res = await fetch("http://localhost:3000/api/user");
   const data = await res.json();
